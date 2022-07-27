720f60814ec16578389b66e80edc6df374fc7079
Bojan Ristic <e.tachka@gmail.com>
2022-07-27 18:21:33 +0200

    dict

---
+++ dict/dict.go
@@ -0,0 +1,169 @@
+package dict
+
+import (
+	"github.com/obiloud/curry-go/list"
+	"github.com/obiloud/curry-go/maybe"
+	"github.com/obiloud/curry-go/nub"
+	"github.com/obiloud/curry-go/tuple"
+)
+
+type Dict[A nub.Ord, B any] struct {
+	dict list.List[tuple.Tuple[A, B]]
+}
+
+func (dict Dict[A, B]) String() string {
+	return dict.dict.String()
+}
+
+func FromList[A nub.Ord, B any](ls list.List[tuple.Tuple[A, B]]) Dict[A, B] {
+	return Dict[A, B]{dict: list.SortBy(tuple.First[A, B], ls)}
+}
+
+func Empty[A nub.Ord, B any]() Dict[A, B] {
+	return Dict[A, B]{dict: list.Nil[tuple.Tuple[A, B]]()}
+}
+
+func IsEmpty[A nub.Ord, B any](dict Dict[A, B]) bool {
+	return list.IsEmpty(dict.dict)
+}
+
+func Singleton[A nub.Ord, B any](key A, value B) Dict[A, B] {
+	return Dict[A, B]{dict: list.Singleton(tuple.Pair(key, value))}
+}
+
+func Keys[A nub.Ord, B any](dict Dict[A, B]) list.List[A] {
+	return list.Map(tuple.First[A, B], dict.dict)
+}
+
+func Values[A nub.Ord, B any](dict Dict[A, B]) list.List[B] {
+	return list.Map(tuple.Second[A, B], dict.dict)
+}
+
+func ToList[A nub.Ord, B any](dict Dict[A, B]) list.List[tuple.Tuple[A, B]] {
+	return dict.dict
+}
+
+func Get[A nub.Ord, B any](key A, dict Dict[A, B]) maybe.Maybe[B] {
+	return maybe.Map(tuple.Second[A, B],
+		list.Head(list.Filter(func(pair tuple.Tuple[A, B]) bool {
+			return tuple.First(pair) == key
+		}, dict.dict)),
+	)
+}
+
+func Insert[A nub.Ord, B any](key A, value B, dict Dict[A, B]) Dict[A, B] {
+	return FoldR(func(k A, v B, acc Dict[A, B]) Dict[A, B] {
+		if !list.Member(k, Keys(acc)) {
+			return Dict[A, B]{dict: list.SortBy(tuple.First[A, B], list.Cons(tuple.Pair(k, v), acc.dict))}
+		}
+		return acc
+	}, Singleton(key, value), dict)
+}
+
+func Remove[A nub.Ord, B any](key A, dict Dict[A, B]) Dict[A, B] {
+	return Dict[A, B]{dict: list.Filter(func(pair tuple.Tuple[A, B]) bool { return tuple.First(pair) != key }, dict.dict)}
+}
+
+func Update[A nub.Ord, B any](key A, updateFunc func(maybe.Maybe[B]) maybe.Maybe[B], dict Dict[A, B]) Dict[A, B] {
+	return maybe.WithDefault(
+		Remove(key, dict),
+		maybe.Map(nub.Curry(func(key A, value B) Dict[A, B] {
+			return Insert(key, value, dict)
+		})(key), updateFunc(Get(key, dict))),
+	)
+}
+
+func Member[A nub.Ord, B any](key A, dict Dict[A, B]) bool {
+	return list.Member(key, Keys(dict))
+}
+
+func Size[A nub.Ord, B any](dict Dict[A, B]) int {
+	return list.Length(dict.dict)
+}
+
+func Union[A nub.Ord, B any](a Dict[A, B], b Dict[A, B]) Dict[A, B] {
+	return FoldR(Insert[A, B], a, b)
+}
+
+func Intersect[A nub.Ord, B any](a Dict[A, B], b Dict[A, B]) Dict[A, B] {
+	return Filter(func(key A, _ B) bool {
+		return Member(key, b)
+	}, a)
+}
+
+func Diff[A nub.Ord, B any](a Dict[A, B], b Dict[A, B]) Dict[A, B] {
+	return FoldR(func(key A, _ B, acc Dict[A, B]) Dict[A, B] {
+		return Remove(key, acc)
+	}, a, b)
+}
+
+func Filter[A nub.Ord, B any](isGood func(A, B) bool, dict Dict[A, B]) Dict[A, B] {
+	return FoldR(func(key A, value B, acc Dict[A, B]) Dict[A, B] {
+		if isGood(key, value) {
+			return Insert(key, value, acc)
+		}
+		return acc
+	}, Empty[A, B](), dict)
+}
+
+func Partition[A nub.Ord, B any](isGood func(A, B) bool, dict Dict[A, B]) tuple.Tuple[Dict[A, B], Dict[A, B]] {
+	return FoldL(func(key A, value B, acc tuple.Tuple[Dict[A, B], Dict[A, B]]) tuple.Tuple[Dict[A, B], Dict[A, B]] {
+		insert := func(dict Dict[A, B]) Dict[A, B] {
+			return Insert(key, value, dict)
+		}
+		if isGood(key, value) {
+			return tuple.MapFirst(insert, acc)
+		}
+		return tuple.MapSecond(insert, acc)
+	}, tuple.Pair(Empty[A, B](), Empty[A, B]()), dict)
+}
+
+func Merge[A nub.Ord, B, C, D any](insertLeft func(A, B, D) D, insertBoth func(A, B, C, D) D, insertRight func(A, C, D) D, left Dict[A, B], right Dict[A, C], result D) D {
+	var step func(rKey A, rVal C, acc tuple.Tuple[list.List[tuple.Tuple[A, B]], D]) tuple.Tuple[list.List[tuple.Tuple[A, B]], D]
+
+	step = func(rKey A, rVal C, acc tuple.Tuple[list.List[tuple.Tuple[A, B]], D]) tuple.Tuple[list.List[tuple.Tuple[A, B]], D] {
+		xs := tuple.First(acc)
+
+		if list.IsEmpty(xs) {
+			return tuple.MapSecond(func(r D) D {
+				return insertRight(rKey, rVal, r)
+			}, acc)
+		}
+
+		return maybe.WithDefault(
+			acc,
+			maybe.Map2(func(head tuple.Tuple[A, B], tail list.List[tuple.Tuple[A, B]]) tuple.Tuple[list.List[tuple.Tuple[A, B]], D] {
+				lKey := tuple.First(head)
+				lValue := tuple.Second(head)
+
+				if lKey < rKey {
+					return step(rKey, rVal, tuple.Pair(tail, insertLeft(lKey, lValue, tuple.Second(acc))))
+				} else if lKey > rKey {
+					return tuple.MapSecond(func(r D) D {
+						return insertRight(rKey, rVal, r)
+					}, acc)
+				}
+
+				return tuple.Pair(tail, insertBoth(lKey, lValue, rVal, tuple.Second(acc)))
+
+			}, list.Head(xs), list.Tail(xs)))
+	}
+
+	intermediate := FoldL(step, tuple.Pair(left.dict, result), right)
+
+	return list.FoldL(func(pair tuple.Tuple[A, B], acc D) D {
+		return insertLeft(tuple.First(pair), tuple.Second(pair), acc)
+	}, tuple.Second(intermediate), tuple.First(intermediate))
+}
+
+func FoldL[A nub.Ord, B any, C any](fn func(A, B, C) C, acc C, dict Dict[A, B]) C {
+	return list.FoldL(func(pair tuple.Tuple[A, B], acc C) C {
+		return fn(tuple.First(pair), tuple.Second(pair), acc)
+	}, acc, dict.dict)
+}
+
+func FoldR[A nub.Ord, B any, C any](fn func(A, B, C) C, acc C, dict Dict[A, B]) C {
+	return list.FoldR(func(pair tuple.Tuple[A, B], acc C) C {
+		return fn(tuple.First(pair), tuple.Second(pair), acc)
+	}, acc, dict.dict)
+}
---
+++ dict/dict_test.go
@@ -0,0 +1,136 @@
+package dict
+
+import (
+	"testing"
+
+	"github.com/obiloud/curry-go/list"
+	"github.com/obiloud/curry-go/maybe"
+	"github.com/obiloud/curry-go/nub"
+	"github.com/obiloud/curry-go/tuple"
+)
+
+func TestBuild(t *testing.T) {
+	if FromList(list.Nil[tuple.Tuple[string, string]]()) != Empty[string, string]() {
+		t.Error("Empty")
+	}
+
+	if FromList(list.Singleton(tuple.Pair("k", "v"))) != Singleton("k", "v") {
+		t.Error("Singleton")
+	}
+
+	if Singleton("k", "v") != Insert("k", "v", Empty[string, string]()) {
+		t.Error("Insert")
+	}
+
+	if Singleton("k", "b") != Insert("k", "b", Singleton("k", "a")) {
+		t.Error("Insert replace")
+	}
+
+	updateWithValueFunc := nub.Const[maybe.Maybe[string], maybe.Maybe[string]](maybe.Just("b"))
+
+	if Singleton("k", "b") != Update("k", updateWithValueFunc, Singleton("k", "a")) {
+		t.Error("Update")
+	}
+
+	updateWithNothingFunc := nub.Const[maybe.Maybe[string], maybe.Maybe[string]](maybe.Nothing[string]())
+
+	if Empty[string, string]() != Update("k", updateWithNothingFunc, Singleton("k", "v")) {
+		t.Error("Update Nothing")
+	}
+
+	if Empty[string, string]() != Remove("k", Singleton("k", "v")) {
+		t.Error("Remove")
+	}
+
+	if Singleton("k", "v") != Remove("foo", Singleton("k", "v")) {
+		t.Error("Remove not found")
+	}
+}
+
+var animals = FromList(list.Cons(tuple.Pair("Tom", "cat"), list.Singleton(tuple.Pair("Jerry", "mouse"))))
+
+func TestQuery(t *testing.T) {
+	if !Member("Tom", animals) {
+		t.Error("Member 1")
+	}
+
+	if Member("Spike", animals) {
+		t.Error("Member 2")
+	}
+
+	if Get("Tom", animals) != maybe.Just("cat") {
+		t.Error("Get 1")
+	}
+
+	if Get("Spike", animals) != maybe.Nothing[string]() {
+		t.Error("Get 2")
+	}
+
+	if Size(Empty[string, string]()) != 0 {
+		t.Error("Size of empty")
+	}
+
+	if Size(animals) != 2 {
+		t.Error("Size of example")
+	}
+}
+
+func TestCombine(t *testing.T) {
+	if Union(Singleton("Jerry", "mouse"), Singleton("Tom", "cat")) != animals {
+		t.Errorf("Union")
+	}
+
+	if Union(Singleton("Tom", "cat"), Singleton("Tom", "cat")) != Singleton("Tom", "cat") {
+		t.Errorf("Union collision")
+	}
+
+	if Intersect(Singleton("Tom", "cat"), animals) != Singleton("Tom", "cat") {
+		t.Error("Intersect")
+	}
+
+	if Diff(animals, Singleton("Tom", "cat")) != Singleton("Jerry", "mouse") {
+		t.Error("Diff")
+	}
+}
+
+func TestTransform(t *testing.T) {
+	if Filter(func(k string, _ string) bool { return k == "Tom" }, animals) != Singleton("Tom", "cat") {
+		t.Error("Filter")
+	}
+
+	if Partition(func(k string, _ string) bool { return k == "Tom" }, animals) != tuple.Pair(Singleton("Tom", "cat"), Singleton("Jerry", "mouse")) {
+		t.Error("Partition")
+	}
+}
+
+func insertBoth[T nub.Ord](key T, valueLeft list.List[int], valueRight list.List[int], dict Dict[T, list.List[int]]) Dict[T, list.List[int]] {
+	return Insert(key, list.Append(valueLeft, valueRight), dict)
+}
+
+func TestMerge(t *testing.T) {
+
+	if Merge(
+		Insert[int, list.List[int]],
+		insertBoth[int],
+		Insert[int, list.List[int]],
+		Empty[int, list.List[int]](),
+		Empty[int, list.List[int]](),
+		Empty[int, list.List[int]](),
+	) != Empty[int, list.List[int]]() {
+		t.Error("Merge empties")
+	}
+
+	s1 := Insert("u1", list.Singleton(1), Empty[string, list.List[int]]())
+	s2 := Insert("u2", list.Singleton(2), Empty[string, list.List[int]]())
+
+	if ToList(Merge(
+		Insert[string, list.List[int]],
+		insertBoth[string],
+		Insert[string, list.List[int]],
+		s1,
+		s2,
+		Empty[string, list.List[int]](),
+	)) != list.FromSlice([]tuple.Tuple[string, list.List[int]]{tuple.Pair("u1", list.Singleton(1)), tuple.Pair("u2", list.Singleton(2))}) {
+		t.Error("Merge singletons in order")
+	}
+}
