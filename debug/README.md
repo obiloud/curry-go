556839b4d7aaf9a946d1a0141e919a83b8ccf3ac
Bojan Ristic <e.tachka@gmail.com>
2023-04-28 15:41:51 +0200

    use env variable to suppress debugging

---
+++ debug/README.md
@@ -0,0 +1,3 @@
+# Debug
+
+To enable debugging set the environment variable GLOBAL_DEBUG_ENABLED to a non-empty string.
--- debug/debug.go
+++ debug/debug.go
@@ -2,11 +2,14 @@ package debug
 
 import (
 	"log"
+	"os"
 
 	"github.com/obiloud/curry-go/util"
 )
 
 func Debug[T any](msg string, a T) T {
-	log.Printf("%s: %s", msg, util.Stringify(a))
+	if os.Getenv("GLOBAL_DEBUG_ENABLED") != "" {
+		log.Printf("%s: %s", msg, util.Stringify(a))
+	}
 	return a
 }
